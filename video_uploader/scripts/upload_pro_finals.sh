0be545a99868da85b828b959014c37c0053c7b4f
Patrick Canny <patrickcanny@ku.edu>
2020-05-08 14:21:11 -0600

    add scripts

---
+++ video_uploader/scripts/upload_pro_finals.sh
@@ -0,0 +1,7 @@
+for file in ../../Open/Pro_Finals/*.mp4; do
+    if [ -f "$file" ]; then
+        echo "$file"
+        echo "python2 ../upload.py --file='$file' --title='$file' --description='Scales Open V4 is Brought to You by the Scales Collective' --keywords='yoyo, scales' --category='22' --privacyStatus='unlisted'"
+        python2 ../upload.py --file="$file" --title="$file" --description="Scales Open V4 is Brought to You by the Scales Collective" --keywords="yoyo, scales" --category="22" --privacyStatus="unlisted"
+    fi
+done
